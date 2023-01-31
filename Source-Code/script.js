20341710100f13378d7a62761276e60473a3baf6
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2023-01-31 20:04:37 +0800

    Modify navmenu-mobile sidebar

--- Source-Code/script.js
+++ Source-Code/script.js
@@ -14,14 +14,17 @@ setInterval(function(){
 const menu = document.querySelector('.menu');
 const sidebar = document.querySelector('.navmenu-mobile');
 const close = document.querySelector('.close');
+const navbar = document.querySelector('.navbar');
 
 
 
 menu.addEventListener('click', () =>{
     sidebar.classList.add('display');
+    navbar.classList.add('hide');
 })
 
 
 close.addEventListener('click', ()=>{
     sidebar.classList.remove('display');
+    navbar.classList.remove('hide');
 })
